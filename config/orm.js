6472d040ca3fb6bbd15998c5600cce28b7951ea3
kknoerz <kevink430@gmail.com>
2016-05-10 15:32:18 -0400

    added remove

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+var connection = require('../config/connection.js');
+
+var orm = {
+  all: function(tableInput, cb) {
+      var queryString = 'SELECT * FROM ' + tableInput + ';';
+      connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          cb(result);
+      });
+  },
+  create: function(table, cols, vals, cb) {
+      var queryString = 'INSERT INTO ' + table;
+
+      queryString = queryString + ' (';
+      queryString = queryString + cols.toString();
+      queryString = queryString + ') ';
+      queryString = queryString + 'VALUES ( ? );';
+
+      console.log(queryString)
+      vals = vals.toString();
+      connection.query(queryString, vals, function(err, result) {
+        if (err) throw err;
+        cb(result);
+      });
+    },
+    update: function(table, objColVals, condition, cb) {
+      console.log('this is objColVals: ', objColVals);
+      if(objColVals.devoured == 'true'){
+        objColVals = 1
+      }else{
+        objColVals = 0
+      }
+      var queryString = 'UPDATE ' + table;
+
+      queryString = queryString + ' SET ';
+      queryString = queryString + 'devoured = '+ objColVals;
+      queryString = queryString + ' WHERE ';
+      queryString = queryString + condition;
+
+      console.log(queryString)
+      connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        cb(result);
+      });
+    }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+-- INSERT INTO clients (client_name) VALUES ('Bilal');
+-- INSERT INTO clients (client_name) VALUES ('Brianne');
+-- INSERT INTO clients (client_name) VALUES ('Vincent');
+--
+-- INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Everybody Loves Raymond', 'tv', 500, 1);
+-- INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Big Bang Theory', 'tv', 900, 1);
+-- INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Top Gun', 'movie', 200, 2);
+-- INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Whiskey', 'grown-up', 300, 2);
+-- INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Cigar', 'grown-up', 250, 3);
+
+-- CREATE TABLE burgers
+-- (
+--   id int NOT NULL AUTO_INCREMENT,
+--   burger_name varchar(255),
+--   devoured tinyint(1),
+--   date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   PRIMARY KEY (id)
+-- );
+
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Bald Eagle Burger');
